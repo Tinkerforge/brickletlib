3971c4747e60ff90063ebf150d06da1283891d65
Olaf Lüke <olaf@tinkerforge.com>
2015-04-30 14:02:10 +0200

    Add debug functions without header

--- bricklet_debug.h
+++ bricklet_debug.h
@@ -30,41 +30,56 @@
 
 #if LOGGING_LEVEL <= LOGGING_DEBUG
 #define logbld(str, ...) do{ BA->printf("<D> bl: " str, ##__VA_ARGS__); }while(0)
+#define logblwohd(str, ...) do{ BA->printf(str, ##__VA_ARGS__); }while(0)
 #else
 #define logbld(str, ...) {}
+#define logblwohd(str, ...) {}
 #endif
 
 #if LOGGING_LEVEL <= LOGGING_INFO
 #define logbli(str, ...) do{ BA->printf("<I> bl: " str, ##__VA_ARGS__); }while(0)
+#define logblwohi(str, ...) do{ BA->printf(str, ##__VA_ARGS__); }while(0)
 #else
 #define logbli(str, ...) {}
+#define logblwohi(str, ...) {}
 #endif
 
 #if LOGGING_LEVEL <= LOGGING_WARNING
 #define logblw(str, ...) do{ BA->printf("<W> bl: " str, ##__VA_ARGS__); }while(0)
+#define logblwohw(str, ...) do{ BA->printf(str, ##__VA_ARGS__); }while(0)
 #else
 #define logblw(str, ...) {}
+#define logblwohw(str, ...) {}
 #endif
 
 #if LOGGING_LEVEL <= LOGGING_ERROR
 #define logble(str, ...) do{ BA->printf("<E> bl: " str, ##__VA_ARGS__); }while(0)
+#define logblwohe(str, ...) do{ BA->printf(str, ##__VA_ARGS__); }while(0)
 #else
 #define logble(str, ...) {}
+#define logblwohe(str, ...) {}
 #endif
 
 #if LOGGING_LEVEL <= LOGGING_FATAL
 #define logblf(str, ...) do{ BA->printf("<F> bl: " str, ##__VA_ARGS__); }while(0)
+#define logblwohf(str, ...) do{ BA->printf(str, ##__VA_ARGS__); }while(0)
 #else
 #define logblf(str, ...) {}
+#define logblwohf(str, ...) {}
 #endif
 
 #else
 
 #define logbld(str, ...) {}
+#define logblwohd(str, ...) {}
 #define logbli(str, ...) {}
+#define logblwohi(str, ...) {}
 #define logblw(str, ...) {}
+#define logblwohw(str, ...) {}
 #define logble(str, ...) {}
+#define logblwohe(str, ...) {}
 #define logblf(str, ...) {}
+#define logblwohf(str, ...) {}
 
 #endif
 
